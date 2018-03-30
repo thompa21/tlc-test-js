ef57430a48e7b8ff8885ef7b9e52e6a8403bd9bc
unknown <thomas.lind@tlcgolfit.se>
2018-03-30 17:33:01 +0200

    commit

--- app/shared/quiz/quiz-page.js
+++ app/shared/quiz/quiz-page.js
@@ -28,19 +28,31 @@ function onNavigatingTo(args) {
     }
     var observableModule = require("data/observable");
     var ObservableArray = require("data/observable-array").ObservableArray;
-    var lista = new ObservableArray();
+    var questions = new ObservableArray();
+    var options = new ObservableArray();
+    var quizlist = new ObservableArray();
+    var quiz = "";
     const context = page.navigationContext;
 
-    http.getJSON("http://tlcgolfit.se/webservices/tlcgolfit/api/v1/quizzes/1?api_key=jbjhvbas56fa865faityvasdfa5f8as5fd8a6scda864s5cd8a4sdc863c861c8136dc1864wq86drc8q6cc1cghfx12gfmoi909").then(function(result) {
+    http.getJSON("http://tlcgolfit.se/webservices/tlcgolfit/api/v1/quizzes/fullquiz/1?api_key=jbjhvbas56fa865faityvasdfa5f8as5fd8a6scda864s5cd8a4sdc863c861c8136dc1864wq86drc8q6cc1cghfx12gfmoi909").then(function(result) {
+        console.dir(result);
+        quizlist = result;
         if(!result.length) {
-            lista.push({id: result.id, name: result.name, description: result.description, quiztype_id: result.quiztype_id });
+            quiz = result.quiz;
+            questions.push(result.questions);
+            options.push(result.options);
         }
         for (i=0;i<result.length;i++) {
-            lista.push({id: result[i].id, name: result[i].name, description: result[i].description, quiztype_id: result[i].quiztype_id });
+            quiz = result[i].quiz;
+            questions.push(result[i].questions);
+            options.push(result[i].options);
         };
         var pageData = new observableModule.fromObject({
-            videoList: lista,
-            quiztype: context.quiztype
+            quiz: quiz,
+            questionList: questions,
+            optionList: options,
+            quiztype: context.quiztype,
+            quizList: quizlist
         });
         const page = args.object;
         page.bindingContext = pageData;
@@ -66,8 +78,23 @@ function submitquiz(args) {
 }
 
 function onPropertyChanged(args) {
+    
     const component = args.object;
-    console.log(component.id);  
+    console.log(component.id);
+    
+    let checkBox = frameModule.topmost().getViewById(component.id);
+    console.log(checkBox.checked);
+    if (checkBox.checked === "false") {
+        checkBox.checked = "true";
+        checkBox.text = String.fromCharCode(0xf058);
+        //0xf14A 0xf192
+    } else  {
+        checkBox.checked = "false";
+        checkBox.text = String.fromCharCode(0xf10C);
+        //0xf096 0xf10C
+    }
+    console.log(checkBox.text);
+
 }
 
 exports.onNavigatingTo = onNavigatingTo;
